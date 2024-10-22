e8b97498c2fe86e3827a96c25f3cf1bb19f3ac2d
Daniel Francisco Puch Ceballos <danielceballos0217@outlook.es>
2024-10-22 00:43:01 -0600

    api change

--- src/app/api/tasks/route.js
+++ src/app/api/tasks/route.js
@@ -3,9 +3,13 @@ import {connectDB} from '@/utils/db'
 import Task from "@/models/Task"
 
 export async function GET(){
-    connectDB()
-    const tasks = await Task.find()
-    return NextResponse.json(tasks)
+    try {
+        connectDB()
+        const tasks = await Task.find()
+        return NextResponse.json(tasks)
+    } catch (error) {
+        return NextResponse.json({ error: 'Error al obtener tareas', details: error.message }, { status: 500 });
+    }
 }
 export async function POST(request){
     try {
